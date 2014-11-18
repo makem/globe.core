779d11e6fdfb2b1b29ff166368c4a36cfbae05a5
Makem <makem@ukr.net>
2014-11-18 18:13:49 +0200

    Added tests

---
+++ tests/messaging/bus.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mavy on 11/18/2014.
+ */
+
+describe('Message bus', function () {
+    it('should send direct message');
+    it('should inform about answer received');
+    it('should propagate a message if no recipients found locally');
+});
